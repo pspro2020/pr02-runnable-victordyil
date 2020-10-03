1975bc78e498841a1be18bc9fc986ba1cfd63af7
Negro <victoryil27oficial@gmail.com>
2020-10-03 14:33:32 +0200

    Final 1

--- src/Elevator.java
+++ src/Elevator.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class Elevator {
+public class Elevator implements Runnable {
+    private final int numb;
+    public Elevator(int numb) {
+        this.numb = numb;
+    }
+    @Override
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            System.out.printf("%s: %d ^ %d = %.0f\n",Thread.currentThread().getName(),numb,i,Math.pow(i,numb));
+        }
+
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,11 @@
 public class Main {
+    public static void main(String[] args) {
+        for (int i = 0; i < 11; i++) {
+            Thread  d = new Thread(new Elevator(i));
+            d.setName("Thread "+i);
+            if (i%2==0) d.setPriority(Thread.MAX_PRIORITY);
+            else d.setPriority(Thread.MIN_PRIORITY);
+            d.start();
+        }
+    }
 }
